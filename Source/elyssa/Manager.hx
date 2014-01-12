2d3301d31ecd6cfe9b90cc3920a1b217fb4d9daf
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-12 23:51:02 +0100

    Added Manager

---
+++ Source/elyssa/Manager.hx
@@ -0,0 +1,51 @@
+package elyssa;
+
+import freezedev.EventMap;
+
+/**
+ * ...
+ * @author ...
+ */
+class Manager<T>
+{
+	private var items: Map<String, T>;
+	public var events: EventMap;
+	
+	public function new()
+	{
+		events = new EventMap();
+		
+		items = new Map<String, T>();
+	}
+	
+	public function add(name: String, item: T)
+	{
+		if (!exists(name)) {	
+			items.set(name, item);
+			
+			events.trigger('add', {
+				name: name,
+				value: item
+			});
+		}
+	}
+	
+	public function remove(name: String)
+	{
+		if (exists(name)) {		
+			items.remove(name);
+			
+			events.trigger('remove', name);
+		}
+	}
+	
+	public function iterator(): Iterator<T>
+	{
+		return items.iterator();
+	}
+	
+	public function exists(name: String)
+	{
+		return items.exists(name);
+	}
+}
