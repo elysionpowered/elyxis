e19f1770f6019a17c663a94851cc0779a425750c
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-13 18:22:40 +0100

    Added draw and events for Entity

--- Source/elyssa/Entity.hx
+++ Source/elyssa/Entity.hx
@@ -1,5 +1,6 @@
 package elyssa;
 import flash.display.Sprite;
+import freezedev.EventMap;
 
 /**
  * ...
@@ -9,6 +10,7 @@ class Entity extends Sprite
 {
 	public var children: EntityManager;
 	public var components: ComponentManager;
+	public var events: EventMap;
 	
 	public function new(?name: String) 
 	{
@@ -33,8 +35,20 @@ class Entity extends Sprite
 		children.add(entity.name, entity);
 	}
 	
+	// TODO: Reflect if a draw is really needed
+	public function draw()
+	{
+		events.trigger('draw');
+		
+		for (e in children) {
+			e.draw();
+		}
+	}
+	
 	public function update(deltaTime: Float)
 	{
+		events.trigger('update', deltaTime);
+		
 		for (e in children) {
 			e.update(deltaTime);
 		}
