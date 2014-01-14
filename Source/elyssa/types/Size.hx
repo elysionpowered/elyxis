bab584dd9f2ad8ad60e9d4cedca8b14c723b5ef0
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-14 22:59:21 +0100

    Added size stub

---
+++ Source/elyssa/types/Size.hx
@@ -0,0 +1,18 @@
+package elyssa.types;
+
+/**
+ * ...
+ * @author ...
+ */
+class Size
+{
+	public var width: Int;
+	public var height: Int;
+
+	public function new(width, height) 
+	{
+		this.width = width;
+		this.height = height;
+	}
+	
+}
