7b0bdd460d04e704bb84753515d6f218dfcfe5f2
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-21 20:17:00 +0100

    Updated scene to use camera

--- Source/elyxis/Scene.hx
+++ Source/elyxis/Scene.hx
@@ -7,11 +7,15 @@ package elyxis;
 class Scene extends GameObject
 {
 	public var game: Game;
+	public var camera: Camera
 	public var guiLayer: Layer;
 
 	public function new() 
 	{
 		super();
+		
+		camera = new Camera();
+		guiLayer = new Layer();
 	}
 	
 }
