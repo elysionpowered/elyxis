1ea2707cea9da98aee60b83c83a74aabdf2e973e
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-22 23:31:22 +0100

    Added vector2f type stub

---
+++ Source/elyxis/types/Vector2f.hx
@@ -0,0 +1,18 @@
+package elyxis.types;
+
+/**
+ * ...
+ * @author ...
+ */
+class Vector2f
+{
+	public var x: Float;
+	public var y: Float;
+
+	public function new(x: Float = 0.0, y: Float = 0.0) 
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+}
