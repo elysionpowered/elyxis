9069ab1ff91e7355aa40653fcc642892d0038b04
Johannes Stein <johannesstein@freeze-dev.com>
2014-02-23 22:46:25 +0100

    Game loop is set up

--- Source/elyxis/Game.hx
+++ Source/elyxis/Game.hx
@@ -1,6 +1,8 @@
 package elyxis;
 import elyxis.types.Size;
 import flash.display.Sprite;
+import flash.events.Event;
+import flash.Lib;
 
 /**
  * ...
@@ -11,6 +13,7 @@ class Game extends Sprite
 	public var director: SceneDirector;
 	public var viewport: Viewport;
 	public var screen(get, never): Size;
+	private var lastTick: Float;
 	
 	function get_screen() {
 		return new Size(stage.stageWidth, stage.stageHeight);
@@ -21,10 +24,24 @@ class Game extends Sprite
 	{
 		super();
 		
+		lastTick = 0;
+		
 		viewport = new Viewport();
 		this.addChild(viewport);
 		
 		director = new SceneDirector(viewport, this);
+		
+		this.addEventListener(Event.ENTER_FRAME, function(e) {
+			var delta = Lib.getTimer() - lastTick;
+			update(delta);
+			lastTick = Lib.getTimer();
+		});
+	}
+	
+	public function update(dt) {
+		if (director.currentScene != null) {
+			director.currentScene.update(dt);
+		}
 	}
 	
 }
